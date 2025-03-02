74411a3624d29b81294880ba5e54c2cf35a36640
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-02 01:24:43 +0100

    navbar dropdowns

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -12,24 +12,66 @@ const Navbar = () => {
         setMenuActive( menuActive== false ? true: false)
     }
   return (
-    <nav>
+    <nav className='flex bg-[#fff9f2] w-full  justify-start md:justify-center items-center'>
         
-        <div>
+        <div className=' md:hidden relative'>
             <Image src={menuActive ? 'cancel.svg': 'menu.svg'} alt="toggle" onClick={handleToggle} width={50} height={50}/>
-            {}
+            {menuActive && (
+                <ul className='absolute w-screen'>
+                    <li>
+                        <Link href="/">HOME</Link>
+                    </li>
+                    <li>
+                        <Link href="/brides">BRIDES</Link>
+                    </li>
+                    <li>
+                        <Link href="/non-bridals">NON BRIDALS</Link>
+                    </li>
+                    <li>
+                        <Link href="/about-us">ABOUT US</Link>
+                    </li>
+                    <li>
+                        <Link href="/make-an-enquiry">MAKE AN ENQUIRY</Link>
+                    </li>
+                </ul>
+            )
+             }
         </div>
         
 
-        <div>
+        <div className=' md:flex md:flex-col md:justify-center md:items-center'>
             <Link href="/">
                 <Image src={logo} alt="Logo" className='w-[250px] '/>
             </Link>
-            <ul>
+            <ul className='hidden md:flex space-x-2'>
                 <li>
                     <Link href="/">HOME</Link>
                 </li>
                 <li>
-                    <Link href="/brides">BRIDES</Link>
+                    <span>BRIDE</span>
+                    <ul>
+                        <li>
+                            <span>COLLECTIONS <Image src="right.svg" alt="right" width={30} height={30}/></span>
+                            <div>
+                                <p><Link href='/gutsy-collection'>THE GUTSY COLLECTION LOOKBOOK</Link></p>
+                            </div>
+                        </li>
+                        <li>
+                            <span>THE PROCESS <Image src="right.svg" alt="right" width={30} height={30}/></span>
+                            <div>
+                                <p><Link href="/bespoke">BESPOKE</Link></p>
+                                <p><Link href="/made-to-order">MADE TO ORDER</Link></p>
+                            </div>
+
+                        </li>
+                        <li>
+                            <span>ORDER OUR COLLECTION <Image src="right.svg" alt="right" width={30} height={30}/></span>
+                            <div>
+                                <p><Link href="/make-an-enquiry">MAKE AN ENQUIRY</Link></p>    
+                            </div>
+
+                        </li>
+                    </ul>
                 </li>
                 <li>
                     <Link href="/non-bridals">NON BRIDALS</Link>
