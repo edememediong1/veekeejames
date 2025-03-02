e408ddf3789ff7678100ca9f6e93ed1f3dc068db
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-03 00:35:58 +0100

    hero one

--- app/Components/HomePage/Hero.tsx
+++ app/Components/HomePage/Hero.tsx
@@ -4,11 +4,10 @@ import Link from 'next/link'
 const Hero = () => {
   return (
     <div>
-        <section className='bg-[url("https://veekeejames.com/wp-content/uploads/2023/12/Bridal-new.jpg")] bg-center bg-cover md:bg-[url("https://veekeejames.com/wp-content/uploads/2023/12/Bridal-desktop.jpg")] h-[100vh] md:bg-center bg-no-repeat md:bg-cover'>
-            <article>
+        <section className='bg-[url("https://veekeejames.com/wp-content/uploads/2023/12/Bridal-new.jpg")] bg-center bg-cover md:bg-[url("https://veekeejames.com/wp-content/uploads/2023/12/Bridal-desktop.jpg")] h-[100vh] md:bg-center bg-no-repeat md:bg-cover text-white flex flex-col justify-end items-center md:justify-center md:items-start'>
+            <article className="flex flex-cols justify-center items-center">
                 <h2>THE GUSTY COLLECTION</h2>
-                
-                <button>
+                <button className=''>
                     <Link href="">View Collection</Link>
                 </button>
             </article>
