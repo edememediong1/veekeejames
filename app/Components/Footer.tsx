dd909b915896eff75ab027029f94e7fca63be0b6
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-06 08:08:43 +0100

    footer #2

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import facebook from '../../public/facebook.svg'
+import whatsapp from '../../public/whatsapp.svg'
+// import { Facebook, PhoneIcon as Whatsapp } from 'lucide-react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex flex-col md:gap-5 md:justify-start lg:flex-row w-full lg:justify-around   bg-black text-white p-7 gap-10 text-[13px]'>
+        <ul className="flex flex-col md:flex-row gap-4 ">
+            <li className='border-b py-2 md:border-none'>About us</li>
+            <li className='border-b py-2 md:border-none'>Bridal collection</li>
+            <li className='border-b py-2 md:border-none'>Book an appointment</li>
+            <li className='border-b py-2 md:border-none'>Make an Enquiry</li>
+            <li className='border-b py-2 md:border-none'>Contact us</li>
+
+        </ul>
+        <section className='flex flex-col md:flex-col lg-flex gap-5'>
+            <p>&copy;2025 Flora Fashions</p>
+            <span className='flex gap-3'>
+                <Link href="">
+                    <Image src={facebook} alt="facebook logo" width={20} height={20} className='bg-white'/>
+                </Link>
+                <Link href="">
+                    <Image src={whatsapp} alt="whatsapp logo" width={20} height={20} className=' bg-white'/>
+                </Link>
+            </span>
+        </section>
+    </footer>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./Components/Navbar";
 import { Montserrat } from "next/font/google";
+import Footer from "./Components/Footer";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
       >
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
