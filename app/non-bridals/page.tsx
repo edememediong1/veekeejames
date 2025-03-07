e5764f3302f43cfa8b6d8d517cbcac6e56c0e8d3
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-07 22:08:53 +0100

    non-bridal form component

--- app/non-bridals/page.tsx
+++ app/non-bridals/page.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+
 const page = () => {
   return (
     <main>
         <div className="h-[70vh] md:h-[400px] lg:h-[100vh] bg-[#0c0c0c] w-full flex justify-center items-center p-5">
             <h1 className='text-white text-3xl text-center font-medium'>NON-BRIDAL DRESSES</h1>
         </div>
+
+        <form>
+          
+        </form>
 
     </main>
   )
