808148ad412cd9254194689425beffb01440e124
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-09 23:26:04 +0100

    added the first radio selections

--- app/Components/NonBridals/Form.tsx
+++ app/Components/NonBridals/Form.tsx
@@ -14,8 +14,21 @@ const Form = () => {
         </div>
 
         <div>
-            <label htmlFor="email"></label>
-            <input type='email' name="email" placeholder='user@example.com' />
+            <label htmlFor="phone">Your Whatsapp number. (this will be your primary contact detail with us, please enter correctly or we may not be able to reach you) <span className='text-red-500'>*</span></label>
+            <input type="tel" name="phone" placeholder='' />
+        </div>
+
+        <div>
+          <p>Which service are you interested in? <span className='text-red-500'>*</span></p>
+          <span>
+            <input type="radio" value="Bespoke Enquiry White Wedding Dress" id="wedding" name='service'/>
+            <label htmlFor="wedding">Bespoke Enquiry White Wedding Dress</label>
+          </span>
+          <span>
+            <input type="radio" value="Bespoke Enquiry for Bridal Dresses" id="bridal" name='service'/>
+            <label htmlFor="bridal">Bespoke Enquiry Other bridal dresses  &rbrack; e.g. tradtional, reception, after party &lbrack;</label>
+          </span>
+          
         </div>
 
     </form>
