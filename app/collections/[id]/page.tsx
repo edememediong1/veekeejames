19bec11d4b1d4dd76795af3a93aeba0fb9507aaa
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-18 21:21:15 +0100

    added  styles to the button

--- app/collections/[id]/page.tsx
+++ app/collections/[id]/page.tsx
@@ -23,15 +23,15 @@ const Page = async ({params} : ImageDetailPageProp) => {
   
   return(
     <div>
-       <section>
+       <section className="">
           <Link href="/collections">
-              <button>
+              <button className='flex '>
                 <ArrowLeft className='w-4 h-4 mr-2' />
                 Back to Gallery
               </button>
           </Link>
        </section>
-       <section className="flex flex-cols gap-10 md:flex-row md:w-[80%] m-auto">
+       <section className="flex flex-cols gap-14 md:flex-row md:w-[80%] m-auto my-16">
           {image ? (
             <div className='relative w-[50%] h-[100vh]'>
                 <Image
@@ -47,11 +47,16 @@ const Page = async ({params} : ImageDetailPageProp) => {
             <p>Image not found</p>
           )}
           { image && (
-            <article className='w-[50%]'>
-              <h1>GTY-0<span>{image.id}</span></h1>
+            <article className='w-[50%] flex flex-col gap-10 items-left'>
+              <h1 className='font-medium text-2xl'>GTY-0<span>{image.id}</span></h1>
               <p>{image.description}</p>
-              <button>Make an Enquiry</button>
-          </article>)}
+              <Link href="/make-an-enquiry">
+                  <button 
+                    className='bg-black text-white p-2 w-[180px]'>
+                      Make an Enquiry
+                  </button>
+              </Link>
+            </article>)}
        </section>
 
     </div>
