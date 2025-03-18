35e10015515cacfb1045a8c39b5cbdfece19b6ef
Emediong Bassey Edem <edememediong1@gmail.com>
2025-03-18 03:42:11 +0100

    added loading and not found ppages

--- app/collections/[id]/page.tsx
+++ app/collections/[id]/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
 
 const page = () => {
   return (
     <div>
-      
+       <section>
+          <article>
+              <Image/>
+          </article>
+          <article>
+
+          </article>
+       </section>
 
     </div>
   )
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>Page not found oooo</div>
+  )
+}
+
+export default NotFound
