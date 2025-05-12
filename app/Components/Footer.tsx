a8e7f500831bcc6c8feda2305c6976279d198a17
Emediong Bassey Edem <edememediong1@gmail.com>
2025-05-13 00:48:46 +0100

    fix: added social media links

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -3,11 +3,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 import facebook from '../../public/facebook.svg'
 import whatsapp from '../../public/whatsapp.svg'
+import twitter from '../../public/twitter.svg'
+import instagram from '../../public/instagram.svg'
 // import { Facebook, PhoneIcon as Whatsapp } from 'lucide-react'
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col md:gap-5 md:justify-start lg:flex-row w-full lg:justify-around   bg-black text-white p-7 gap-10 text-[13px]'>
+    <footer className='flex flex-col md:gap-5 md:justify-start lg:flex-row w-full lg:justify-around lg:h-[30vh] lg:items-center  bg-black text-white p-7 gap-10 text-[13px]'>
         <ul className="flex flex-col md:flex-row gap-4 ">
             <li className='border-b py-2 md:border-none'>
                 <Link href='/about-us'>About Us</Link>
@@ -22,14 +24,20 @@ const Footer = () => {
                 <Link href='/contact'>Contact Us</Link></li>
 
         </ul>
-        <section className='flex flex-col md:flex-col lg-flex gap-5'>
+        <section className='flex flex-col md:flex-col lg:flex-row gap-5 lg:gap-12'>
             <p>&copy;2025 Flora Fashions</p>
             <span className='flex gap-3'>
                 <Link href="">
-                    <Image src={facebook} alt="facebook logo" width={20} height={20} className='bg-white'/>
+                    <Image src={facebook} alt="facebook logo" width={18} height={18} className=''/>
                 </Link>
                 <Link href="">
-                    <Image src={whatsapp} alt="whatsapp logo" width={20} height={20} className=' bg-white'/>
+                    <Image src={whatsapp} alt="whatsapp logo" width={18} height={18} className=''/>
+                </Link>
+                <Link href="">
+                    <Image src={twitter} alt="twitter logo" width={18} height={18} className=''/>
+                </Link>
+                <Link href="">
+                    <Image src={instagram} alt="twitter logo" width={18} height={18} className=''/>
                 </Link>
             </span>
         </section>
