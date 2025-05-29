8335ea084695f22088e2244d5ef4dad6dec92939
Emediong Bassey Edem <edememediong1@gmail.com>
2025-05-29 13:16:19 +0100

    update: updated the favicon

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,18 +1,14 @@
 'use client'
 import Link from 'next/link'
 import Image from 'next/image'
-// import facebook from '../../public/facebook.svg'
-// import whatsapp from '../../public/whatsapp.svg'
-// import twitter from '../../public/twitter.svg'
-// import instagram from '../../public/instagram.svg'
-// import { Facebook, PhoneIcon as Whatsapp } from 'lucide-react'
+
 
 const Footer = () => {
     const whatsappNumber = "+2348169499444"
     const whatsappMessage = "Hello Flora! I'm interested in your services.\nHere's what I want..."
     const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
   return (
-    <footer className='flex flex-col md:gap-5 md:justify-start lg:flex-row w-full lg:justify-around lg:h-[30vh] lg:items-center  bg-black text-white p-7 gap-10 text-[13px]'>
+    <footer className='flex flex-col uppercase md:gap-5 md:justify-start lg:flex-row w-full lg:justify-around lg:h-[30vh] lg:items-center  bg-black text-white p-7 gap-10 text-[13px]'>
         <ul className="flex flex-col md:flex-row gap-4 ">
             <li className='border-b border-[#4f4f4f] py-2 md:border-none'>
                 <Link href='/about-us'>About Us</Link>
@@ -28,7 +24,7 @@ const Footer = () => {
 
         </ul>
         <section className='flex flex-col md:flex-col lg:flex-row gap-5 lg:gap-12'>
-            <p>&copy; {Date()} LuxuryByFlora</p>
+            <p>&copy; {new Date().getFullYear()} LUXURY BY FLORA</p>
             <span className='flex gap-3'>
                 <Link  href="https://www.facebook.com/share/1ByT26Q4HV/?mibextid=wwXIfr" target='_blank'>
                     <Image src='facebook.svg' alt="facebook logo" width={18} height={18} className='text-[#4f4f4f]'/>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Navbar from "./Components/Navbar";
 import { Montserrat } from "next/font/google";
 import Footer from "./Components/Footer";
-import Icon from "./assets/flora_logo.png"
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -22,12 +21,11 @@ const montserrat = Montserrat({
 })
 
 export const metadata: Metadata = {
-  title: "Flora ",
-  description: "Established in 2022, Flora has become a symbol of timeless elegance, creativity, and refined femininity. With every piece we create, we aim to transform not just appearances, but presence — sculpting silhouettes that tell bold, beautiful stories. Renowned for our ability to reimagine and elevate the women we dress, each custom design is a statement of confidence, grace, and individuality made for every season.",
+  title: "LUXURY BY FLORA",
+  description: "Established in 2023, Flora has become a symbol of timeless elegance, creativity, and refined femininity. With every piece we create, we aim to transform not just appearances, but presence — sculpting silhouettes that tell bold, beautiful stories. Renowned for our ability to reimagine and elevate the women we dress, each custom design is a statement of confidence, grace, and individuality made for every season.",
   icons: {
-    icon: `${Icon}`
+    icon: "/flora_icon.png", 
   }
-  
 };
 
 
